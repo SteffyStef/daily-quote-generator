d2e44c3d9c43db804cdc875610f3646f425b407d
Stefani Stephens <runtsaquarius@gmail.com>
2025-06-10 20:04:48 -0700

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/quote-generator/', // <-- your repo name!
+  base: '/quote-generator/', // ⚠️ This must match your GitHub repo name
   plugins: [react()],
 });
 
